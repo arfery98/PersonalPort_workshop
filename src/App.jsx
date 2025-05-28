7ff24c70e8dee5c49cb30ad53e14f2b169907baf
AFERY_RS <arferyrs@gmail.com>
2025-05-28 21:18:26 +0700

    setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { LoadingScreen } from './components/LoadingScreen'
+function App() {
+
+
+  return (
+    <>
+      <LoadingScreen />
+    </>
+  )
+}
+
+export default App
