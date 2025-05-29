97affc0e49650d452ed797251ba858917d084968
AFERY_RS <arferyrs@gmail.com>
2025-05-29 23:45:03 +0700

    home section responsive intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import { useState } from 'react'
 import './App.css'
+import { useState } from 'react'
 import { LoadingScreen } from './components/LoadingScreen'
+import { Navbar } from './components/Navbar'
+import { MobileMenu } from './components/MobileMenu'
 import "./index.css"
 
 function App() {
   const [isLoaded, setLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setLoaded(true)} />}{" "}
-      <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+      <div
+        className={`min-h-screen transition-opacity duration-700 
+      ${isLoaded ? "opacity-100" : "opacity-0"} 
+      bg-black text-gray-100`}
+      >
 
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       </div>
     </>
-  )
+  );
 }
 
 export default App;
